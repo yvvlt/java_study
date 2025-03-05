8e7c94c3503e3caf820fff54d5512d6639d58095
yvvlt <yewon5260@naver.com>
2025-03-05 23:04:45 +0900

    Add files via upload

---
+++ 수업/src/src/Problem_06.java
@@ -0,0 +1,14 @@
+public class Problem_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("byte 형의 크기\t\t ==> %d\n", Byte.SIZE);
+		System.out.printf("byte 형의 크기\t\t ==> %d\n", Short.SIZE);
+		System.out.printf("byte 형의 크기\t\t ==> %d\n", Integer.SIZE);
+		System.out.printf("byte 형의 크기\t\t ==> %d\n", Long.SIZE);
+		System.out.printf("byte 형의 크기\t\t ==> %d\n", Float.SIZE);
+		System.out.printf("byte 형의 크기\t\t ==> %d\n", Double.SIZE);
+		System.out.printf("byte 형의 크기\t\t ==> %d\n", Character.SIZE);
+	}
+
+}
