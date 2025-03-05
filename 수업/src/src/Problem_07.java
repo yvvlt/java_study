801bbb3ef85ef0402f8d47456d7c4c27eca2f6ce
yvvlt <yewon5260@naver.com>
2025-03-05 23:15:15 +0900

    Add files via upload

---
+++ 수업/src/src/Problem_07.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Problem_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		String str;
+		int i;
+		
+		System.out.print("문자열을 입력 ==> ");
+		str = s.nextLine();
+		
+		for (i=str.length()-1;i>=0;i--) {
+			System.out.printf("%c", str.charAt(i));
+		}
+		System.out.printf("\n");
+	}
+}
